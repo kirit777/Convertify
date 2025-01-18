486120c0b27575fa1814422eb92a74c4b51c3d27
Gareja Kirit <garejakirit@gmail.com>
2025-01-18 12:46:42 +0530

    Added Project for first time

---
+++ Convertify/Convertify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Convertify
+//
+//  Created by HKinfoway Tech. on 09/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
